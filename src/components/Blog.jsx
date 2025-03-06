9d2e4ec6e0a16fa6b26ded3ec81205f4468ddebf
Aayush SInha <84586770+aayushsinha0706@users.noreply.github.com>
2025-03-06 16:08:35 +0530

    5.7 Blog List Frontend, step 7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,42 @@
+import { useState } from 'react'
 const Blog = ({blog}) => {
+
+  const [view, setView] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleView = () => {
+    setView(!view)
+  }
+  
     return (
-      <div>
-        {blog.title}{' '}{blog.author}
+      <div style={blogStyle}>
+        {
+          view
+           ? <div>
+                <div>
+                  {blog.title} {blog.author}
+                  <button onClick={toggleView}>Hide</button>
+                </div>
+                <a href={blog.url}>{blog.url}</a>
+                <div>
+                  likes: {blog.likes}
+                  <button>like</button>
+                </div>
+                <div>
+                  {blog.user.name}
+                </div>  
+              </div>
+            : <div>
+                <p>{blog.title}{' '}{blog.user.name}</p>
+                <button onClick={toggleView}>view</button>
+              </div>
+         }
       </div>
     )
 }
