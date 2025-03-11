196bfa723c6910d6f469532bb6bc4f7991fab4c5
Aayush SInha <84586770+aayushsinha0706@users.noreply.github.com>
2025-03-11 15:31:45 +0530

    5.14: Blog List Tests, step 2 && 5.15: Blog List Tests, step 3

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -4,7 +4,7 @@ import Blog from './Blog'
 
 describe('<Blog />', () => {
 
-    let container
+    let container, likeBlog
 
     beforeEach(() => {
         const blog = {
@@ -25,7 +25,9 @@ describe('<Blog />', () => {
             "name": "Example User",
         }
 
-        container = render(<Blog blog={blog} user={user} />).container
+        likeBlog = vi.fn()
+
+        container = render(<Blog blog={blog} user={user} likeBlog={likeBlog} />).container
     })
 
     test('5.13 Blog renders title and author but not URL and likes by default', async () => {
@@ -35,5 +37,30 @@ describe('<Blog />', () => {
         expect(button).toHaveTextContent('view')
         expect(div).not.toHaveTextContent('https://example.com')
         expect(div).not.toHaveTextContent('likes: 0')
+    })
+
+    test("5.14 blog's URL and number of likes are shown when button view is clicked", async () => {
+        const user = userEvent.setup()
+        const button = screen.getByRole('button')
+        await user.click(button)
+
+        const div = container.querySelector('.blog')
+        expect(div).toHaveTextContent('example blog exampleUser')
+        expect(div).toHaveTextContent('https://example.com')
+        expect(div).toHaveTextContent('likes: 0')
+        expect(div).toHaveTextContent('Example User')
+    })
+
+    test("If the like button is clicked twice, the event handler is called twice", async () => {
+        let user = userEvent.setup()
+        const button = screen.getByText('view')
+        await user.click(button)
+
+        user = userEvent.setup()
+        const likeButton = screen.getByText('like')
+        await user.click(likeButton)
+        await user.click(likeButton)
+
+        expect(likeBlog.mock.calls).toHaveLength(2)
     })
 })
